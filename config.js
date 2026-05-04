7dffc857a5234bcef4d3e3bba6ab1c74704e9a84
Tryb3-Tunchi <158272804+Tryb3-Tunchi@users.noreply.github.com>
2026-05-04 09:30:20 +0100

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// ⚠️  This file is in .gitignore — never committed to GitHub
-// Your Gemini API key — keep this private
-
-// export const GEMINI_API_KEY = "AIzaSyDNbDgEyYlafVyMV29QwVNnla6SOFS5l0s";
-
-export const GEMINI_API_KEY = "AIzaSyAP35GSvLrcGgChFMYtg5qfQ4bimiyJBok";
